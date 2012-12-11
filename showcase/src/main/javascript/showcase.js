709c5a853c3dea981e9542122c512f4b90ff7f33
Dmytro Khaynatskyy <Dmytro.Khaynatskyy@hp.com>
2012-12-11 02:20:15 +0100

    Hello AJAX

--- showcase/src/main/javascript/showcase.js
+++ showcase/src/main/javascript/showcase.js
@@ -3,7 +3,9 @@ steal('jquery',
 			$(document).ready(function($) {
 				$(".waitscreen").fadeOut(500,function(){
 						$(this).remove();
-						$(document.body).html("Hello world");
+						$.ajax("/users/ralf/tags").done(function(data){
+							$(document.body).html("Ralf's tags: "+JSON.stringify(data));
+						});
 				});
 			});
 		}
