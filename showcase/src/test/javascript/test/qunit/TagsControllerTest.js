c9ceecfd5c147b1e50a5762014127057dcb64860
Dmytro Khaynatskyy <Dmytro.Khaynatskyy@hp.com>
2013-02-28 01:57:43 +0100

    Add test

--- showcase/src/test/javascript/test/qunit/TagsControllerTest.js
+++ showcase/src/test/javascript/test/qunit/TagsControllerTest.js
@@ -6,6 +6,8 @@ steal("funcunit/qunit",
 	module("common_ux_js_lib_internal HPText");
 
 	test("HPText test", function(el){
-		equal("test","test");
+		var elem = $("<div></div>").appendTo( $("#qunit-test-area") );
+		new TagsController(elem,{name:"ralf"});
+		elem.remove()
 	});
 });
